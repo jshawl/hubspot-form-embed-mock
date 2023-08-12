42ffec9aa0ac81a8afaee44f36005b1b53926a45
Jesse Shawl <jesse@jesse.sh>
2023-08-12 10:26:05 -0500

    Improve test coverage

--- src/hbspt.test.ts
+++ src/hbspt.test.ts
@@ -1,10 +1,40 @@
-import {hbspt} from "./hbspt";
-import {describe, expect, test} from '@jest/globals';
+import { hbspt } from "./hbspt";
+import { describe, expect, test, jest, beforeEach } from "@jest/globals";
 
-describe('hbspt module', () => {
-  test('create() adds a form to the DOM', () => {
+describe("hbspt module", () => {
+  beforeEach(() => {
+    document.body.innerHTML = "";
+  });
+  test("create() adds a form to the DOM", () => {
     expect(document.body.querySelector("form")).toBeFalsy();
-    hbspt.forms.create({})
+    hbspt.forms.create({});
     expect(document.body.querySelector("form")).toBeTruthy();
+  });
+  test("onFormReady", () => {
+    const onFormReady = jest.fn();
+    hbspt.forms.create({
+      onFormReady,
+    });
+    expect(onFormReady).toHaveBeenCalled();
+  });
+  test("onFormSubmit is called before validation", () => {
+    const onFormSubmit = jest.fn();
+    hbspt.forms.create({
+      onFormSubmit,
+    });
+    document.querySelector("input")?.setAttribute("value", "");
+    document.querySelector("form")?.submit();
+    expect(onFormSubmit).toHaveBeenCalled();
+  });
+  test("onFormSubmitted is called after validation", () => {
+    const onFormSubmitted = jest.fn();
+    hbspt.forms.create({
+      onFormSubmitted,
+    });
+    document
+      .querySelector("input")
+      ?.setAttribute("value", "example@example.org");
+    document.querySelector("form")?.submit();
+    expect(onFormSubmitted).toHaveBeenCalled();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { hbspt } from './hbspt';
+import { hbspt } from "./hbspt";
 
 window.hbspt = hbspt;
