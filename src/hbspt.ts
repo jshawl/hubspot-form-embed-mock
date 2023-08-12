4865f062e2cf45de5efc7c51ff8c4a2c152dc097
Jesse Shawl <jesse@jesse.sh>
2023-08-12 10:27:53 -0500

    Improve test coverage (#2)
    * Improve test coverage
    
    * remove console.log

--- src/hbspt.ts
+++ src/hbspt.ts
@@ -5,31 +5,31 @@ interface Options {
 }
 
 export const hbspt = {
-    forms: {
-      create: ({ onFormReady, onFormSubmit, onFormSubmitted }: Options) => {
-        const form = document.createElement("form");
-        const input = document.createElement("input");
-        const submit = document.createElement("button");
-        submit.innerHTML = "Submit";
-        submit.setAttribute("type", "submit");
-        input.setAttribute("type", "email");
-        form.appendChild(input);
-        form.appendChild(submit);
-  
-        form.addEventListener("submit", (e) => {
-          e.preventDefault();
-          if( onFormSubmit )
-            onFormSubmit(form);
-          if (!input.value) {
-            return false;
-          }
-          if( onFormSubmitted )
-            onFormSubmitted(form);
-        });
-  
-        document.body.appendChild(form);
-        if( onFormReady )
-          onFormReady(form);
-      },
+  forms: {
+    create: ({ onFormReady, onFormSubmit, onFormSubmitted }: Options) => {
+      const form = document.createElement("form");
+      const input = document.createElement("input");
+      const submit = document.createElement("button");
+      submit.innerHTML = "Submit";
+      submit.setAttribute("type", "submit");
+      input.setAttribute("type", "email");
+      form.appendChild(input);
+      form.appendChild(submit);
+
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (onFormSubmit) {
+          onFormSubmit(form);
+        }
+
+        if (!input.value) {
+          return false;
+        }
+        if (onFormSubmitted) onFormSubmitted(form);
+      });
+
+      document.body.appendChild(form);
+      if (onFormReady) onFormReady(form);
     },
-  }
+  },
+};
