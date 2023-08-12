6f94423b90551d9699a1e3d8ac07f06cee652e9f
Jesse Shawl <jesse@jesse.sh>
2023-08-12 13:09:06 -0500

    Add Implementation (#5)
    * Add Implementation
    
    * Refactor
    
    * import type

--- src/hbspt.test.ts
+++ src/hbspt.test.ts
@@ -1,40 +1,148 @@
-import { hbspt } from "./hbspt";
+import { hbspt, type Options } from "./hbspt";
 import { describe, expect, test, jest, beforeEach } from "@jest/globals";
+
+const options: Options = {
+  portalId: "",
+  formId: "",
+};
+
+const submitForm = (valid = true) => {
+  document
+    .querySelector("input")
+    ?.setAttribute("value", valid ? "example@example.org" : "");
+  document.querySelector("form")?.submit();
+};
 
 describe("hbspt module", () => {
   beforeEach(() => {
     document.body.innerHTML = "";
   });
+
   test("create() adds a form to the DOM", () => {
     expect(document.body.querySelector("form")).toBeFalsy();
-    hbspt.forms.create({});
+    hbspt.forms.create({
+      ...options,
+    });
     expect(document.body.querySelector("form")).toBeTruthy();
   });
+
+  test("target", () => {
+    const id = btoa(Math.random().toString()).substring(10, 15);
+    document.body.innerHTML = `<div id="${id}"></div>`;
+    hbspt.forms.create({
+      ...options,
+      target: `#${id}`,
+    });
+    expect(document.body.querySelector(`#${id} form`)).toBeTruthy();
+  });
+
+  test("cssClass", () => {
+    const cssClass = btoa(Math.random().toString()).substring(10, 15);
+    hbspt.forms.create({
+      ...options,
+      cssClass,
+    });
+    expect(
+      document.body.querySelector("form")?.classList.contains(cssClass),
+    ).toBe(true);
+  });
+
+  test("submitButtonClass", () => {
+    const submitButtonClass = btoa(Math.random().toString()).substring(10, 15);
+    hbspt.forms.create({
+      ...options,
+      submitButtonClass,
+    });
+    expect(
+      document.body
+        .querySelector("form input[type='submit']")
+        ?.classList.contains(submitButtonClass),
+    ).toBe(true);
+  });
+
+  test("submitText", () => {
+    const submitText = "Submit, please!";
+    hbspt.forms.create({
+      ...options,
+      submitText,
+    });
+    const submit = document.body.querySelector(
+      "input[type='submit']",
+    ) as HTMLInputElement;
+    expect(submit?.value).toBe(submitText);
+  });
+
+  test("inlineMessage", () => {
+    const inlineMessage = "Thank you!";
+    hbspt.forms.create({
+      ...options,
+      inlineMessage,
+    });
+    submitForm();
+    expect(document.body.innerHTML).toEqual(inlineMessage);
+  });
+
+  test("redirectUrl", () => {
+    Object.defineProperty(window, "location", {
+      value: {
+        href: "/",
+      },
+      writable: true,
+    });
+    const redirectUrl = "https://example.org/";
+    hbspt.forms.create({
+      ...options,
+      redirectUrl,
+    });
+    submitForm();
+    expect(window.location.href).toEqual(redirectUrl);
+  });
+
+  test("onBeforeFormInit", () => {
+    const onBeforeFormInit = jest.fn();
+    hbspt.forms.create({
+      ...options,
+      onBeforeFormInit,
+    });
+    expect(onBeforeFormInit).toHaveBeenCalled();
+  });
+
   test("onFormReady", () => {
     const onFormReady = jest.fn();
     hbspt.forms.create({
+      ...options,
       onFormReady,
     });
     expect(onFormReady).toHaveBeenCalled();
   });
+
+  test("onBeforeFormSubmit", () => {
+    const onBeforeFormSubmit = jest.fn();
+    hbspt.forms.create({
+      ...options,
+      onBeforeFormSubmit,
+    });
+    document.querySelector("form")?.submit();
+    expect(onBeforeFormSubmit).toHaveBeenCalled();
+  });
+
   test("onFormSubmit is called before validation", () => {
     const onFormSubmit = jest.fn();
     hbspt.forms.create({
+      ...options,
       onFormSubmit,
     });
-    document.querySelector("input")?.setAttribute("value", "");
-    document.querySelector("form")?.submit();
+    submitForm(false);
     expect(onFormSubmit).toHaveBeenCalled();
   });
+
   test("onFormSubmitted is called after validation", () => {
     const onFormSubmitted = jest.fn();
     hbspt.forms.create({
+      ...options,
       onFormSubmitted,
     });
-    document
-      .querySelector("input")
-      ?.setAttribute("value", "example@example.org");
-    document.querySelector("form")?.submit();
+    submitForm();
     expect(onFormSubmitted).toHaveBeenCalled();
   });
 });
--- src/hbspt.ts
+++ src/hbspt.ts
@@ -1,39 +1,105 @@
-interface Options {
+export interface Options {
+  portalId: number | string;
+  formId: string;
+  region?: string;
+  target?: string;
+  redirectUrl?: string;
+  inlineMessage?: string;
+  pageId?: string;
+  cssRequired?: string;
+  cssClass?: string;
+  css?: string;
+  submitText?: string;
+  submitButtonClass?: string;
+  errorClass?: string;
+  errorMessageClass?: string;
+  locale?: string;
+  translations?: Record<string, unknown>;
+  manuallyBlockedEmailDomain?: string;
+  formInstanceId?: string;
+  sfdcCampaignId?: string;
+  goToWebinarWebinarKey?: string;
+  onBeforeFormInit?: (context: Options) => void;
   onFormReady?: (form: HTMLFormElement) => void;
   onFormSubmit?: (form: HTMLFormElement) => void;
+  onBeforeFormSubmit?: (form: HTMLFormElement) => void;
   onFormSubmitted?: (form: HTMLFormElement) => void;
 }
 
 export const hbspt = {
   forms: {
-    create: ({ onFormReady, onFormSubmit, onFormSubmitted }: Options) => {
+    create: (options: Options) => {
+      if (options.onBeforeFormInit) {
+        options.onBeforeFormInit(options);
+      }
+
+      let container: HTMLElement | null;
+      if (options.target) {
+        container = document.querySelector(options.target);
+      } else {
+        container = document.body;
+      }
+
       const form = document.createElement("form");
+      if (options.cssClass) {
+        form.classList.add(options.cssClass);
+      }
+
       const input = document.createElement("input");
-      const submit = document.createElement("button");
+      input.setAttribute("type", "email");
+
+      const submit = document.createElement("input");
+      submit.setAttribute("type", "submit");
+      if (options.submitButtonClass) {
+        submit.classList.add(options.submitButtonClass);
+      }
+      if (options.submitText) {
+        submit.value = options.submitText;
+      } else {
+        submit.value = "Submit";
+      }
+
       const label = document.createElement("label");
       label.innerHTML =
         "<span>Email</span><span class='hs-form-required'>*</span>";
-      submit.innerHTML = "Submit";
-      submit.setAttribute("type", "submit");
-      input.setAttribute("type", "email");
+
       form.appendChild(label);
       form.appendChild(input);
       form.appendChild(submit);
 
       form.addEventListener("submit", (e) => {
         e.preventDefault();
-        if (onFormSubmit) {
-          onFormSubmit(form);
+
+        if (options.onBeforeFormSubmit) {
+          options.onBeforeFormSubmit(form);
+        }
+
+        if (options.onFormSubmit) {
+          options.onFormSubmit(form);
         }
 
         if (!input.value) {
           return false;
         }
-        if (onFormSubmitted) onFormSubmitted(form);
+
+        if (options.onFormSubmitted) {
+          options.onFormSubmitted(form);
+        }
+
+        if (options.inlineMessage && container) {
+          container.innerHTML = options.inlineMessage;
+        }
+
+        if (options.redirectUrl) {
+          window.location.href = options.redirectUrl;
+        }
       });
 
-      document.body.appendChild(form);
-      if (onFormReady) onFormReady(form);
+      container?.appendChild(form);
+
+      if (options.onFormReady) {
+        options.onFormReady(form);
+      }
     },
   },
 };
